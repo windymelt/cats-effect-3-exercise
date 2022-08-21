e4c97a57cc759edd73b5cbf8f2b3733731c6e409
Windymelt <windymelt@3qe.us>
2022-08-21 23:09:49 +0900

    cute animation

--- build.sbt
+++ build.sbt
@@ -7,13 +7,15 @@ ThisBuild / organizationName := "example"
 
 addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
 
-lazy val root = (project in file("."))
+lazy val root = (project in file(".")).enablePlugins(NativeImagePlugin)
   .settings(
     name := "cats-effect-3-exercise",
     libraryDependencies ++= Seq(
       "org.typelevel" %% "cats-effect" % "3.3.12",
       scalaTest % Test
-    )
+    ),
+    nativeImageOptions  += "--no-fallback",
+    nativeImageVersion  := "22.1.0"
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -7,37 +7,47 @@ import scala.concurrent.Future
 // IO.asyncやIO.shiftの効果を調べたい
 
 object Hello extends IOApp.Simple with AsynchronousComputation with Download {
+  import scala.concurrent.duration._
+  import scala.language.postfixOps
   val cpuCount = IO(Runtime.getRuntime().availableProcessors())
 
   val run = for {
-    // cnt <- cpuCount
-    // _ <- IO.println(s"Available #CPU: ${cnt}")
-    // _ <- IO.println("Running tremendous jobs synchronously...")
-    // // Compute用のThread pool[io-compute-*]が利用される
-    // // ThreadはCore数いっぱい生成され、使いまわされる
-    // _ <- List.range(0, cnt * 2).parTraverse(n => doSomething(n).debug)
-    // _ <- IO.println("Now, we are going to compute on pool for blocking I/O...")
-    // _ <- List
-    //   .range(0, cnt * 2)
-    //   .parTraverse(n =>
-    //     IO.blocking {
-    //       Thread.sleep((Math.random() * 1000).toLong)
-    //       println(s"(inside blocking)[${Thread.currentThread().getName()}]")
-    //       n
-    //     }
-    //   ) // debugはここでは使えない(debug自体はblockingの外で実行されるため)
-    // _ <- IO.println("CE3 doesn't have IO.shift. but it has IO.blocking()")
-    // _ <- IO("one").debug
-    // _ <- IO("two").debug
-    // _ <- IO
-    //   .blocking(s"blocking three: ${Thread.currentThread().getName()}")
-    //   .debug
-    // _ <- IO("four").debug
-    // _ <- IO("five").debug
-    // _ <- IO("six").debug
+    cnt <- cpuCount
+    _ <- IO.println(s"Available #CPU: ${cnt}")
+    _ <- IO.println("Running tremendous jobs synchronously...")
+    _ <- IO.sleep(1 second)
+    _ <- clearScreen
+    // Compute用のThread pool[io-compute-*]が利用される
+    // ThreadはCore数いっぱい生成され、使いまわされる
+    _ <- List.range(0, 2500).parTraverse(n => doSomething(n))
+    _ <- IO.println("Now, we are going to compute on pool for blocking I/O...")
+    _ <- clearScreen
+    _ <- List
+      .range(0, 2500)
+      .parTraverse(i =>
+        IO.blocking {
+          Thread.sleep((Math.random() * 1000).toLong)
+          // println(s"(inside blocking)[${Thread.currentThread().getName()}]")
+          val y = i / 50
+          val x = i % 50
+          print(s"\u001b[$y;${x}H${threadColor}")
+          i
+        }
+      ) // debugはここでは使えない(debug自体はblockingの外で実行されるため)
+    _ <- IO.println("CE3 doesn't have IO.shift. but it has IO.blocking()")
+    _ <- IO("one").debug
+    _ <- IO("two").debug
+    _ <- IO
+      .blocking(s"blocking three: ${Thread.currentThread().getName()}")
+      .debug
+    _ <- IO("four").debug
+    _ <- IO("five").debug
+    _ <- IO("six").debug
     // _ <- IO.fromFuture(IO(k1))
     // _ <- IO.fromFuture(IO(k1))
-    downloadedFile <- backgroundDownloadingIndicator.use { _ => verySlowDownload.debug }
+    downloadedFile <- backgroundDownloadingIndicator.use { _ =>
+      verySlowDownload.debug
+    }
     _ <- IO.println(s"we have downloaded $downloadedFile")
   } yield IO.unit
 
@@ -53,8 +63,19 @@ trait AsynchronousComputation {
     println(s"k1 is running on ${Thread.currentThread().getName()}")
   }
 
+  def clearScreen = IO.print("\u001b[2J")
+  def threadColor = {
+    val n = Thread.currentThread().getId() % 256
+    val r = n % 2 * 255
+    val g = n >> 1
+    s"\u001b[48;2;$r;$g;0m \u001b[0m"
+  }
   def doSomething(i: Int) =
-    IO.sleep(Math.random() * 10 second) >> IO(i)
+    IO.sleep(Math.random() * 10 second) >> IO {
+      val y = i / 50
+      val x = i % 50
+      print(s"\u001b[$y;${x}H${threadColor}")
+    }
 
   implicit class IOShow[A](io: IO[A]) {
     def debug(): IO[A] = for {
@@ -68,12 +89,19 @@ trait Download {
   import scala.concurrent.duration._
   import scala.language.postfixOps
 
-  def verySlowDownload: IO[String] = IO.println("Starting Download") >> IO.sleep(3 second) >> IO.delay {
-    // Finally we got result
-    "/tmp/foobar.txt"
-  }
+  def verySlowDownload: IO[String] =
+    IO.println("Starting Download") >> IO.sleep(3 second) >> IO.delay {
+      // Finally we got result
+      "/tmp/foobar.txt"
+    }
 
-  def backgroundDownloadingIndicator: ResourceIO[IO[OutcomeIO[Unit]]] = downloadingIndicator.background
+  def backgroundDownloadingIndicator: ResourceIO[IO[OutcomeIO[Unit]]] =
+    downloadingIndicator.background
   def downloadingIndicator: IO[Unit] =
-    IO.sleep(100 milliseconds) *> (IO.print("\r|") *> IO.sleep(100 milliseconds) *> IO.print("\r/") *> IO.sleep(100 milliseconds) *> IO.print("\r-") *> IO.sleep(100 milliseconds) *> IO.print("\r\\") *> IO.sleep(100 milliseconds)).foreverM
+    IO.sleep(100 milliseconds) *> (IO.print("\r|") *> IO.sleep(
+      100 milliseconds
+    ) *> IO.print("\r/") *> IO.sleep(100 milliseconds) *> IO.print("\r-") *> IO
+      .sleep(100 milliseconds) *> IO.print("\r\\") *> IO.sleep(
+      100 milliseconds
+    )).foreverM
 }
